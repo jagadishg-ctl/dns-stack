fdb82e8b4be2d3064a65326c49a28306f28c59dc
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-16 11:36:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
 
+
+resource "google_project_service" "dns" {
+  project            = var.project_id
+  service            = "dns.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Read VM stack outputs from GCS remote state (points to your VM stack)
+data "terraformdata "terraform_remote_state" "vm" {
+  backend = "gcs"
+  config = {
+    bucket = "migration_gcs"
+    prefix = "state/stack-vm"
+  }
+}
+
+module "dns" {
+  source            = "./modules/dns"   # <-- DNS module folder lives inside gcp-dns-stack/modules/dns
+  project_id        = var.project_id
+  managed_zone_name = var.managed_zone_name
+  dns_name          = var.dns_name
+  vm_public_ip      = data.terraform_remote_state.vm.outputs.vm_external_ip
+
+  depends_on = [google_project_service.dns]
