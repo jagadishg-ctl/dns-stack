916daff8ab27f7eb2de187c99e6c4c5630b86481
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-16 11:35:42 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+
+variable "project_id" { type = string }
+variable "region"     { type = string }
+variable "zone"       { type = string }
+
+variable "impersonate_service_account" { type = string }
+
+variable "managed_zone_name" { type = string }
